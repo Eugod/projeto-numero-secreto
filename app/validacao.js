ee06de43698d3057d8ef4f4aff7ac5886879cc73
Eugênio Rodrigues <eugenio28.04@gmail.com>
2023-04-06 11:12:26 -0300

    adicionando validação

---
+++ app/validacao.js
@@ -0,0 +1,32 @@
+function verificaValorValido() {
+    const numero = Number(chute);
+
+    if (chuteInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return;
+    }
+
+    if(numeroMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        return;
+    }
+    
+    if(numero == numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>`
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
